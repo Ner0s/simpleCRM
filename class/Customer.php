a3ab30e2a7ba91274f6a2987891fe20bd2451eed
Lukas Friedl <lukasfriedl@live.de>
2021-08-09 08:03:54 +0200

    Outsourced the while statement for creating record arrays;

--- class/Customer.php
+++ class/Customer.php
@@ -10,15 +10,8 @@ class Customer
         $this->conn = $db;
     }
 
-    public function listCustomer()
+    public function createRecords($result)
     {
-        $sqlQuery = "SELECT * FROM " . $this->customerTable;
-
-        $stmt = $this->conn->prepare($sqlQuery);
-        $stmt->execute();
-        $result = $stmt->get_result();
-
-        $displayRecords = $result->num_rows;
         $records = array();
         while ($customer = $result->fetch_assoc()) {
             $rows = array();
@@ -37,6 +30,21 @@ class Customer
             $rows[] = '<button type="button" name="delete" id="' . $customer["customer_nr"] . '" class="btn btn-danger btn-xs delete" ><span class="glyphicon glyphicon-remove" title="Delete"></span></button>';
             $records[] = $rows;
         }
+
+        return $records;
+    }
+
+    public function listCustomer()
+    {
+        $sqlQuery = "SELECT * FROM " . $this->customerTable;
+
+        $stmt = $this->conn->prepare($sqlQuery);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        $displayRecords = $result->num_rows;
+
+        $records = $this->createRecords($result);
 
         $output = array(
             "draw" => intval($_POST["draw"]),
@@ -73,24 +81,8 @@ class Customer
             $result = $stmt->get_result();
 
             $displayRecords = $result->num_rows;
-            $records = array();
-            while ($customer = $result->fetch_assoc()) {
-                $rows = array();
-                $rows[] = $customer['customer_nr'];
-                $rows[] = ucfirst($customer['first_name']);
-                $rows[] = ucfirst($customer['last_name']);
-                $rows[] = $customer['company'];
-                $rows[] = $customer['address'];
-                $rows[] = $customer['city'];
-                $rows[] = $customer['country'];
-                $rows[] = $customer['zip'];
-                $rows[] = $customer['phone'];
-                $rows[] = $customer['email'];
-                $rows[] = $customer['create_date'];
-                $rows[] = '<button type="button" name="update" id="' . $customer["customer_nr"] . '" class="btn btn-warning btn-xs update"><span class="glyphicon glyphicon-edit" title="Edit"></span></button>';
-                $rows[] = '<button type="button" name="delete" id="' . $customer["customer_nr"] . '" class="btn btn-danger btn-xs delete" ><span class="glyphicon glyphicon-remove" title="Delete"></span></button>';
-                $records[] = $rows;
-            }
+
+            $records = $this->createRecords($result);
 
             $output = array(
                 "draw" => intval($_POST["draw"]),
